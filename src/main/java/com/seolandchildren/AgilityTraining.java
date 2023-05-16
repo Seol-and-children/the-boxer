9e14437daecfb522405fcb11dbc5109914c2ae99
busker362 <68013292+busker362@users.noreply.github.com>
2023-05-16 13:44:51 +0900

    [기능] 민첩 훈련 기능구현

---
+++ src/main/java/com/seolandchildren/AgilityTraining.java
@@ -0,0 +1,14 @@
+package com.seolandchildren;
+
+public class AgilityTraining {
+    public void agilityTraining(){
+        Player player = new Player();
+
+        System.out.println("플레이어의 현재 민첩 스탯 :" + player.getAgility());
+        System.out.println("야구공 피하기 훈련을 합니다.. \n 민첩 스탯이 2 성장합니다.");
+
+        player.setAgility(player.getAgility()+2);
+        System.out.println("플레이어의 현재 민첩 스탯 :" + player.getAgility());
+    }
+
+}
