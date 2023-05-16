744894b44c44e960fba5174aa8697ca00eaaef77
busker362 <68013292+busker362@users.noreply.github.com>
2023-05-16 15:49:10 +0900

    [페이지] 훈련 페이지

---
+++ src/main/java/com/seolandchildren/Training.java
@@ -0,0 +1,41 @@
+package com.seolandchildren;
+
+import java.util.Scanner;
+
+public class Training {
+    public static void main(String[] args) {
+        AttackTraining attackTraining = new AttackTraining();
+        AgilityTraining agilityTraining = new AgilityTraining();
+        HealthTraining healthTraining = new HealthTraining();
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("===============  훈련 목록입니다. =============== ");
+            System.out.println("어떤 훈련을 하시겠습니까?");
+            System.out.println("1. 공격 훈련을 하겠다!");
+            System.out.println("2. 민첩 훈련을 하겠다!");
+            System.out.println("3. 체력 훈련을 하겠다!");
+            System.out.println("4. 훈련을 하지 않겠다!");
+            System.out.println("=============== =============== =============== ");
+            System.out.print("무슨 훈련을 하시겠습니까? : ");
+            int no = sc.nextInt();
+            switch (no){
+                case 1 :
+                    attackTraining.attackTraining();
+                    return;
+                case 2 :
+                    agilityTraining.agilityTraining();
+                    return;
+                case 3 :
+                    healthTraining.healthTraining();
+                    return;
+                case 4:
+                    return;
+                default:
+                    System.out.println("잘못입력하셨습니다 다시 입력하세요");
+                    break;
+            }
+        }while (true);
+
+    }
+}
